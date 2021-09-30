b65804e039fef397eebb72e950b29452b9ea08d0
Mariano <marianomarchionna8@gmail.com>
2021-09-30 11:10:18 +0200

    Header links

---
+++ src/data/Links.js
@@ -0,0 +1,27 @@
+const headerLinks = [
+    {
+        text: 'Home',
+        url: '#'
+    },
+    {
+        text: 'Apple',
+        url: '#'
+    },
+    {
+        text: 'Microsoft',
+        url: '#'
+    },
+    {
+        text: 'Android',
+        url: '#'
+    },
+    {
+        text: 'Forum',
+        url: '#'
+    },
+    {
+        text: 'Contact us',
+        url: '#'
+    }
+];
+export default { headerLinks };
